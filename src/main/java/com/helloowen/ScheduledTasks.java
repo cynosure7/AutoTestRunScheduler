f1cc70b192b065f8a2bbb43fe41502707cf47aa0
Owen Cao <owen.cao@sparkventures.co.nz>
2017-05-25 21:49:59 +1200

    initial commit

---
+++ src/main/java/com/helloowen/ScheduledTasks.java
@@ -0,0 +1,7 @@
+package com.helloowen;
+
+/**
+ * Created by Owen Cao on 5/25/17.
+ */
+public class ScheduledTasks {
+}
---
+++ src/main/java/com/helloowen/domain/BambooBuildResponse.java
@@ -0,0 +1,8 @@
+package com.helloowen;
+
+/**
+ * Created by Owen Cao on 5/25/17.
+ */
+public class BambooBuildResults {
+    BambooBuildResults
+}
